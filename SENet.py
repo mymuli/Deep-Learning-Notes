83aad84214eafbc33cf3168aba73030354c6958b
mymuli <37521721+mymuli@users.noreply.github.com>
2021-05-14 19:59:30 +0800

    add SENet

---
+++ SENet.py
@@ -0,0 +1,34 @@
+# encoding: utf-8
+import copy
+import itertools
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.utils.model_zoo as model_zoo
+import random
+from scipy.spatial.distance import cdist
+from sklearn.preprocessing import normalize
+from torch import nn, optim
+from torch.utils.data import dataloader
+from torchvision import transforms
+from torchvision.models.resnet import Bottleneck, resnet50
+from torchvision.transforms import functional
+
+# SENet
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+                nn.Linear(channel, channel // reduction),
+                nn.ReLU(inplace=True),
+                nn.Linear(channel // reduction, channel),
+                nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
