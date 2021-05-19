034ec443e98ce6cd872dee69a986d39bb3d2e0fe
mymuli <37521721+mymuli@users.noreply.github.com>
2021-05-19 15:57:26 +0800

    Update Conv-BN-ReLU.py

--- Conv-BN-ReLU.py
+++ Conv-BN-ReLU.py
@@ -5,7 +5,13 @@ import torch.tensor as tensor
 from torch.nn import functional as F
 
 self.feature = nn.Sequential(
-                  nn.Conv2d(in_channels, reduction_dim, kernel_size=1, bias=False),  # 通道维度减少
-                  nn.BatchNorm2d(reduction_dim),  # BN操作
+                  nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),  # 通道维度减少
+                  nn.BatchNorm2d(out_channels),  # BN操作
                   nn.ReLU(inplace=True) )  # ReLU操作
+
+
+
+
+
+
 
