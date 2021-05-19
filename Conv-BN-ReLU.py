e0c797fe95e2009211f8f519069226e597f411b2
mymuli <37521721+mymuli@users.noreply.github.com>
2021-05-19 15:56:40 +0800

    Update and rename Conv-BN-ReLU to Conv-BN-ReLU.py

--- Conv-BN-ReLU.py
+++ Conv-BN-ReLU.py
@@ -1,3 +1,9 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.tensor as tensor
+from torch.nn import functional as F
+
 self.feature = nn.Sequential(
                   nn.Conv2d(in_channels, reduction_dim, kernel_size=1, bias=False),  # 通道维度减少
                   nn.BatchNorm2d(reduction_dim),  # BN操作
