017978bd7f53369bf769146836550fa14603cf32
mymuli <37521721+mymuli@users.noreply.github.com>
2021-06-17 16:17:10 +0800

    Create PSPModule.py

---
+++ PSPModule.py
@@ -0,0 +1,20 @@
+class PSPModule(nn.Module):
+    # (1, 2, 3, 6)
+    def __init__(self, sizes=(1, 3, 6, 8), dimension=2):
+        super(PSPModule, self).__init__()
+        self.stages = nn.ModuleList([self._make_stage(size, dimension) for size in sizes])
+
+    def _make_stage(self, size, dimension=2):
+        if dimension == 1:
+            prior = nn.AdaptiveAvgPool1d(output_size=size)
+        elif dimension == 2:
+            prior = nn.AdaptiveAvgPool2d(output_size=(size, size))
+        elif dimension == 3:
+            prior = nn.AdaptiveAvgPool3d(output_size=(size, size, size))
+        return prior
+
+    def forward(self, feats):
+        n, c, _, _ = feats.size()
+        priors = [stage(feats).view(n, c, -1) for stage in self.stages]
+        center = torch.cat(priors, -1)
+        return center
