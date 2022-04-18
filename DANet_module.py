296c3d41c18971edcbafd63a333669b5ac2ac8df
mymuli <37521721+mymuli@users.noreply.github.com>
2022-04-18 16:21:21 +0800

    Add files via upload
    DANet结构的代码

---
+++ DANet_module.py
@@ -0,0 +1,154 @@
+###########################################################################
+# Created by: CASIA IVA
+# Email: jliu@nlpr.ia.ac.cn
+# Copyright (c) 2018
+###########################################################################
+
+import numpy as np
+import torch
+import math
+import torch.nn as nn
+from torch.nn import Module, Sequential, Conv2d, ReLU,AdaptiveMaxPool2d, AdaptiveAvgPool2d, \
+    NLLLoss, BCELoss, CrossEntropyLoss, AvgPool2d, MaxPool2d, Parameter, Linear, Sigmoid, Softmax, Dropout, Embedding
+from torch.nn import functional as F
+from torch.autograd import Variable
+# torch_ver = torch.__version__[:3]
+
+# __all__ = ['PAM_Module', 'CAM_Module']
+
+
+class PAM_Module(Module):
+    """ Position attention module"""
+    #Ref from SAGAN
+    def __init__(self, in_dim):
+        super(PAM_Module, self).__init__()
+        self.chanel_in = in_dim
+
+        self.query_conv = Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.key_conv = Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.value_conv = Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.gamma = Parameter(torch.zeros(1))
+
+        self.softmax = Softmax(dim=-1)
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps( B X C X H X W)
+            returns :
+                out : attention value + input feature
+                attention: B X (HxW) X (HxW)
+        """
+        m_batchsize, C, height, width = x.size()
+        proj_query = self.query_conv(x).view(m_batchsize, -1, width*height).permute(0, 2, 1)
+        proj_key = self.key_conv(x).view(m_batchsize, -1, width*height)
+        energy = torch.bmm(proj_query, proj_key)
+        attention = self.softmax(energy)
+        proj_value = self.value_conv(x).view(m_batchsize, -1, width*height)
+
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(m_batchsize, C, height, width)
+
+        out = self.gamma*out + x
+
+        return out
+
+
+class CAM_Module(Module):
+    """ Channel attention module"""
+    def __init__(self, in_dim):
+        super(CAM_Module, self).__init__()
+        self.chanel_in = in_dim
+
+        self.gamma = Parameter(torch.zeros(1))
+        self.softmax  = Softmax(dim=-1)
+
+    def forward(self,x):
+        """
+            inputs :
+                x : input feature maps( B X C X H X W)
+            returns :
+                out : attention value + input feature
+                attention: B X C X C
+        """
+        m_batchsize, C, height, width = x.size()
+        proj_query = x.view(m_batchsize, C, -1)
+        proj_key = x.view(m_batchsize, C, -1).permute(0, 2, 1)
+        # 矩阵相乘
+        energy = torch.bmm(proj_query, proj_key)    # 通道关系矩阵: [16, 2048, 2048]
+        energy_new = torch.max(energy, -1, keepdim=True)[0].expand_as(energy)-energy
+        # softmax操作
+        attention = self.softmax(energy_new)
+        # 值
+        proj_value = x.view(m_batchsize, C, -1)
+
+        # 矩阵相乘
+        out = torch.bmm(attention, proj_value)
+        out = out.view(m_batchsize, C, height, width)
+       
+        out = self.gamma*out + x
+        return out
+
+
+class DANetHead(nn.Module):
+    def __init__(self, in_channels, out_channels, norm_layer):
+        super(DANetHead, self).__init__()
+        inter_channels = in_channels // 4
+        self.conv5a = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
+                                   norm_layer(inter_channels),
+                                   nn.ReLU())
+        
+        self.conv5c = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
+                                   norm_layer(inter_channels),
+                                   nn.ReLU())
+
+        # 空间注意力模块
+        self.sa = PAM_Module(inter_channels)
+        # 通道注意力模块
+        self.sc = CAM_Module(inter_channels)
+        # Conv ==> BN ==> ReLU
+        self.conv51 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, padding=1, bias=False),
+                                    norm_layer(inter_channels),
+                                    nn.ReLU())
+
+        self.conv52 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, padding=1, bias=False),
+                                    norm_layer(inter_channels),
+                                    nn.ReLU())
+
+        self.conv6 = nn.Sequential(nn.Dropout2d(0.1, False), 
+                                   nn.Conv2d(inter_channels, out_channels, 1))
+
+        self.conv7 = nn.Sequential(nn.Dropout2d(0.1, False), 
+                                   nn.Conv2d(inter_channels, out_channels, 1))
+
+        self.conv8 = nn.Sequential(nn.Dropout2d(0.1, False), 
+                                   nn.Conv2d(inter_channels, out_channels, 1))
+
+    def forward(self, x):
+        # 空间注意力模块
+        feat1 = self.conv5a(x)
+        sa_feat = self.sa(feat1)
+        sa_conv = self.conv51(sa_feat)
+        sa_output = self.conv6(sa_conv)
+
+        # 通道注意力模块
+        # Conv ==> BN ==> ReLU
+        feat2 = self.conv5c(x)
+        # 通道关系
+        sc_feat = self.sc(feat2)
+        # Conv ==> BN ==> ReLU
+        sc_conv = self.conv52(sc_feat)
+        sc_output = self.conv7(sc_conv)
+
+        feat_sum = sa_conv+sc_conv
+        
+        sasc_output = self.conv8(feat_sum)
+
+#        output = [sasc_output]
+#        output.append(sa_output)
+#        output.append(sc_output)
+#        return tuple(output)
+        # 用于模型分类
+        return sasc_output
+
+
