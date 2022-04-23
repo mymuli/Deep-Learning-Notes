a136df475f320c8bea8417fa1a06f8d1ab7cff8c
mymuli <37521721+mymuli@users.noreply.github.com>
2022-04-23 22:05:38 +0800

    Create adjust_learning_rate.py

---
+++ adjust_learning_rate.py
@@ -0,0 +1,9 @@
+def adjust_learning_rate(optimizer, epoch, args):
+    """Decays the learning rate with half-cycle cosine after warmup"""
+    if epoch < args.warmup_epochs:
+        lr = args.lr * epoch / args.warmup_epochs 
+    else:
+        lr = args.lr * 0.5 * (1. + math.cos(math.pi * (epoch - args.warmup_epochs) / (args.epochs - args.warmup_epochs)))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+    return lr
